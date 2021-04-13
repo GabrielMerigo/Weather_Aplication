09ee084f03b66ed6c3d51cbce133c7dbd2570874
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-04-12 21:37:04 -0300

    foi!

---
+++ js/weather.js
@@ -0,0 +1,5 @@
+const APIKey = 'ZofrLjsODdHgCAz4wpQqzXd1t6opYg3o';
+
+const cityUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=PortoAlegre`
+
+fetch(cityUrl)
