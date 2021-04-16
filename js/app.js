ebf4f4191c3ab1ff091f8d785fdcafc9ba21ec64
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-04-15 21:44:16 -0300

    Aplicação Feita, Falta o refactoring

--- js/app.js
+++ js/app.js
@@ -4,17 +4,30 @@ const cityNameContainer = document.querySelector('[data-js="city-name"]')
 const cityTemperatureContainer = document.
   querySelector('[data-js="city-temperature"]');
 const cityCard = document.querySelector('[data-js="cityCard"]');
+const timeIconContainer = document.querySelector('[data-js="time-icon" ]');
+
+let timeImg = document.querySelector('[data-js="time"]')
 
 form.addEventListener('submit', async event => {
   event.preventDefault();
 
   const inputValue = event.target.city.value;
   const [{ LocalizedName, Key }] = await getCityData(inputValue)
-  const [{ WeatherText, Temperature }] = await (await getCityWeather(Key)).json()
+  const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = 
+    await (await getCityWeather(Key)).json()
 
   if (cityCard.classList.contains('d-none')){
     cityCard.classList.remove('d-none')
   }
+
+  if(IsDayTime){
+    timeImg.src = './src/day.svg'
+  } else {
+    timeImg.src = './src/night.svg'
+  }
+
+  const timeIcon = `<img src="src/icons/${WeatherIcon}.svg" />`
+  timeIconContainer.innerHTML = timeIcon; 
 
   cityWeatherContainer.textContent = WeatherText
   cityNameContainer.textContent = LocalizedName
