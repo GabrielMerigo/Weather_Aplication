013e63c2ee692e28a8ba5ced384b51dc267bbec1
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-04-15 21:19:38 -0300

    Renderizando dados da API

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 const form = document.querySelector('[data-js="change-location"]');
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityTemperatureContainer = document.
+  querySelector('[data-js="city-temperature"]');
+const cityCard = document.querySelector('[data-js="cityCard"]');
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', async event => {
   event.preventDefault();
 
   const inputValue = event.target.city.value;
-  console.log(inputValue);
+  const [{ LocalizedName, Key }] = await getCityData(inputValue)
+  const [{ WeatherText, Temperature }] = await (await getCityWeather(Key)).json()
+
+  if (cityCard.classList.contains('d-none')){
+    cityCard.classList.remove('d-none')
+  }
+
+  cityWeatherContainer.textContent = WeatherText
+  cityNameContainer.textContent = LocalizedName
+  cityTemperatureContainer.textContent = Temperature.Metric.Value
+
   form.reset()
 })
--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,11 @@
 const APIKey = 'ZofrLjsODdHgCAz4wpQqzXd1t6opYg3o';
 const baseUrl = 'http://dataservice.accuweather.com'
 
-
 const getCityUrl = cityName =>
   `${baseUrl}/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
 
-const getWeatherURL = ({ Key }) => 
-  `${baseUrl}/currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
+const getWeatherURL = cityKey => 
+  `${baseUrl}/currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br`
 
 const fetchData = async url => {
   try {
@@ -24,12 +23,4 @@ const fetchData = async url => {
 
 const getCityData = cityName => fetchData(getCityUrl(cityName))
 
-
-const getCityWeather = async cityName => {
-  const [cityData] = await getCityData(cityName)
-  const cityWeatherUrl = getWeatherURL(cityData)
-  return await ( await fetch(cityWeatherUrl)).json();
-}
-
-getCityWeather('São Paulo')
-  .then(console.log)
+const getCityWeather = cityKey => fetch(getWeatherURL(cityKey))
